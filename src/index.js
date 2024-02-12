2d61883bf5a831b1f30d1797cee3b08923efe1ce
Yamila Rampello <99457830+YamiRampello@users.noreply.github.com>
2024-02-12 19:18:14 -0300

    Agrega llamado a la API
    Llama a la API y muestra un listado de pokemones

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 /// <reference types="jquery" />
+
+const URL = 'https://pokeapi.co/api/v2';
+
+fetch(`${URL}/pokemon`)
+  .then((respuesta) => respuesta.json())
+  .then((respuestaJSON) => {
+    for (i = 0; i <= 19; i++) {
+      $('ul').append(
+        $(
+          `<li> <a href=${respuestaJSON.results[i].url}> ${respuestaJSON.results[i].name}  </a></li>`
+        )
+      );
+    }
+  });
