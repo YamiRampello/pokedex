a389e0fad86dae49a5c38b416987341be59a868c
Yamila Rampello <99457830+YamiRampello@users.noreply.github.com>
2024-03-18 11:44:50 -0300

    Elimina código duplicado
    Define la URL como variable global para no duplicar

--- src/pokedex.js
+++ src/pokedex.js
@@ -1,13 +1,12 @@
-export function obtenerPokemones(offset = '0', limit = '11') {
-  const URL = 'https://pokeapi.co/api/v2/pokemon';
+const URL = 'https://pokeapi.co/api/v2/pokemon';
 
+export function obtenerPokemones(offset = '0', limit = '11') {
   return fetch(`${URL}?offset=${offset}&limit=${limit}`)
     .then((respuesta) => respuesta.json())
     .then((respuestaJSON) => respuestaJSON.results);
 }
 
 export function obtenerDetallePokemonSeleccionado(nombrePokemon) {
-  const URL = 'https://pokeapi.co/api/v2/pokemon';
   return fetch(`${URL}/${nombrePokemon}`).then((detallePokemon) =>
     detallePokemon.json()
   );
